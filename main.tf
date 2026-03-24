cf665eebfd94c335a7f0519717e34673b767d3bf
null <prasanth@runtimez.com>
2026-03-24 21:18:19 +0000

    feat: added support for the gatekeeper

--- main.tf
+++ main.tf
@@ -51,16 +51,23 @@ variable "argocd_app_version" {
   description = "This is the appVersion of argocd. Example: v2.7.11"
 }
 
+variable "gatekeeper_tag" {
+  type        = string
+  description = "Image tag (SHA or semver) for ghcr.repo.gpkg.io/glueops/gatekeeper.platform.glueops.dev"
+}
+
 
 output "helm_values" {
   value = replace(replace(replace(replace(replace(
     replace(
-      data.local_file.argocd_template.content,
-    "placeholder_tenant_key", var.tenant_key),
-    "placeholder_cluster_environment", var.cluster_environment),
-    "placeholder_argocd_oidc_client_secret_from_dex", var.client_secret),
-    "placeholder_glueops_root_domain", var.glueops_root_domain),
-    "      placeholder_argocd_rbac_policies", var.argocd_rbac_policies),
-    "placeholder_argocd_app_version", var.argocd_app_version
+      replace(
+        data.local_file.argocd_template.content,
+      "placeholder_tenant_key", var.tenant_key),
+      "placeholder_cluster_environment", var.cluster_environment),
+      "placeholder_argocd_oidc_client_secret_from_dex", var.client_secret),
+      "placeholder_glueops_root_domain", var.glueops_root_domain),
+      "      placeholder_argocd_rbac_policies", var.argocd_rbac_policies),
+      "placeholder_argocd_app_version", var.argocd_app_version),
+    "placeholder_gatekeeper_tag", var.gatekeeper_tag
   )
 }
